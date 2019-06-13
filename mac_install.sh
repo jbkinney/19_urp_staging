6670e7a3899de758aa21f604936ff012cffa7a66
Benjamin Harris <bharris@wsbs2017-04.cshl.edu>
2019-06-13 10:10:49 -0400

    Add install file for macs

---
+++ mac_install.sh
@@ -0,0 +1,6 @@
+curl https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -o Miniconda3-latest-MacOSX-x86_64.sh
+mv Miniconda3-latest-MacOSX-x86_64.sh ~/
+bash ~/Miniconda3-latest-MacOSX-x86_64.sh
+source ~/.bash_profile
+conda install --file requirements.txt
+conda install -c anaconda jupyter 
