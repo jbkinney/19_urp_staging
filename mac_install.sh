c893728af7ea8c258a9469da0f9fac16293b742d
Benjamin Harris <bharris@wsbs2017-04.cshl.edu>
2019-06-13 10:23:12 -0400

    Fixed Logomaker Install in mac_install

--- mac_install.sh
+++ mac_install.sh
@@ -2,5 +2,6 @@ curl https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -o M
 mv Miniconda3-latest-MacOSX-x86_64.sh ~/
 bash ~/Miniconda3-latest-MacOSX-x86_64.sh
 source ~/.bash_profile
-conda install --file requirements.txt
+conda install `grep -v logomaker requirements.txt`
 conda install -c anaconda jupyter 
+pip install logomaker
